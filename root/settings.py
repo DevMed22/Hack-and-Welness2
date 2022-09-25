ce7e5a0bfa1a214b2efb3f798acff5f34865ca53
Anas Osama <anasosama914@gmail.com>
2022-09-25 05:35:26 +0200

    SMTP done

--- root/settings.py
+++ root/settings.py
@@ -46,6 +46,13 @@ INSTALLED_APPS = [
 ]
 
 AUTH_USER_MODEL = 'accounts.Hero'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='djangogeeks2@gmail.com'
+EMAIL_HOST_PASSWORD='jvujbwdvyvttpzqv'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+RECIPIENT_ADDRESS='anasosama914@gmail.com'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
